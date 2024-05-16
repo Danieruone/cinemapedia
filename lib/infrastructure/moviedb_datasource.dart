8444edca66545b5d2d69de8dd577d233739a5267
Daniel Mendoza <danieruone@gmail.com>
2024-05-15 20:02:59 -0500

    :hammer: Implement dio instance

--- lib/infrastructure/moviedb_datasource.dart
+++ lib/infrastructure/moviedb_datasource.dart
@@ -1,7 +1,13 @@
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {'api_key': Environment.theMovieDBKey}));
+
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     return [];
