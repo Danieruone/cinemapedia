045c404862ce78cfdf59139ab53629a364a3a9ed
Daniel Mendoza <danieruone@gmail.com>
2024-05-17 14:46:58 -0500

    :hammer: Fix movie db model

--- lib/infrastructure/models/moviedb/movie_moviedb.dart
+++ lib/infrastructure/models/moviedb/movie_moviedb.dart
@@ -32,16 +32,16 @@ class MovieMovieDB {
   });
 
   factory MovieMovieDB.fromJson(Map<String, dynamic> json) => MovieMovieDB(
-        adult: json["adult"],
-        backdropPath: json["backdrop_path"],
+        adult: json["adult"] ?? false,
+        backdropPath: json["backdrop_path"] ?? '',
         genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
         id: json["id"],
         originalLanguage:
             originalLanguageValues.map[json["original_language"]]!,
         originalTitle: json["original_title"],
-        overview: json["overview"],
+        overview: json["overview"] ?? "",
         popularity: json["popularity"]?.toDouble(),
-        posterPath: json["poster_path"],
+        posterPath: json["poster_path"] ?? "",
         releaseDate: DateTime.parse(json["release_date"]),
         title: json["title"],
         video: json["video"],
