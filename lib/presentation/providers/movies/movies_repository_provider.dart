269a0f2e8b14d6cac8f10a8e713933f2f852cf4d
Daniel Mendoza <danieruone@gmail.com>
2024-05-19 16:13:05 -0500

    :sparkles: Add provider implementation

---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,6 @@
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infrastructure/repositories/movies_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final moviesRepositoryProvider =
+    Provider((ref) => MoviesRepositoryImpl(MoviedbDatasource()));
